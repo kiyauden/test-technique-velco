186b0517800a56fe688d44e3a6d3e098763b8fb6
Paul PETIT <p.petit@groupeonepoint.com>
2022-09-06 15:38:39 +0200

    initial commit

---
+++ src/test/java/tech/velco/testtechnique/TestTechniqueApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.velco.testtechnique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTechniqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
